c2f21f1b7b5659c15ccd339d20a54beb77bae9c9
Brian Fallon <brian@brianfallon.com>
2024-02-06 09:38:16 -0500

    Keep all list items the same size when loading. Put values in computed properties and changed opacity to prevent contracting/expanding of list items when data is loading.

--- FootballForecast/FootballForecast/UI/Views/ForecastListView.swift
+++ FootballForecast/FootballForecast/UI/Views/ForecastListView.swift
@@ -31,66 +31,65 @@ struct ForecastListView: View {
                     .foregroundStyle(.gray)
 
                 HStack {
-                    //Note: URL from the API needs https added
-                    if let weather = forecastItem.weather, let condition = weather.condition(), let imageURL = URL(string: condition.icon.replacingOccurrences(of: "//", with: "https://"))  {
+                    //Using Kingfisher to load/cache image
+                    KFImage(imageURL)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
 
-                        //Using Kingfisher to load/cache image
-                        KFImage(imageURL)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 32, height: 32)
-
-                        Text(condition.text)
-                            .font(.subheadline)
-                            .foregroundStyle(Color("forecastBlack"))
-
-                    } else {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle(tint: .gray))
-                            .padding()
-                    }
-                }
-
-                if let weather = forecastItem.weather, let wind = weather.wind(), let temperature = weather.temperature() {
-                    
-                    HStack(spacing:2){
-
-                        Image(systemName: "thermometer.medium")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 16, height: 16)
-
-                        Text("\(Int(temperature))°")
-                            .font(.caption)
-                            .foregroundStyle(Color("forecastBlack"))
-
-                        Text("|")
-                            .font(.caption)
-                            .foregroundStyle(.gray)
-                            .padding([.leading,.trailing], 6)
-
-                        Image(systemName: "wind")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 16, height: 16)
-
-                        Text("\(Int(wind)) mph")
-                            .font(.caption)
-                            .foregroundStyle(Color("forecastBlack"))
-                    }
-
-                    Text("\(forecastItem.stadium.roof.uppercased()) ROOF")
-                        .font(.system(size: 10, weight: .regular, design: .default))
+                    Text(conditionText)
+                        .font(.subheadline)
                         .foregroundStyle(Color("forecastBlack"))
-                        .padding(.top, 4)
+
                 }
+                .opacity(forecastItem.weather == nil ? 0 : 1)
+                .overlay {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                        .padding()
+                        .opacity(forecastItem.weather == nil ? 1 : 0)
+                }
+
+                HStack(spacing:2){
+
+                    Image(systemName: "thermometer.medium")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 16, height: 16)
+
+                    Text(temperature)
+                        .font(.caption)
+                        .foregroundStyle(Color("forecastBlack"))
+
+                    Text("|")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                        .padding([.leading,.trailing], 6)
+
+                    Image(systemName: "wind")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 16, height: 16)
+
+                    Text(wind)
+                        .font(.caption)
+                        .foregroundStyle(Color("forecastBlack"))
+                }
+                .opacity(forecastItem.weather == nil ? 0 : 1)
+
+
+                Text(roof)
+                    .font(.system(size: 10, weight: .regular, design: .default))
+                    .foregroundStyle(Color("forecastBlack"))
+                    .padding(.top, 4)
+                    .opacity(forecastItem.weather == nil ? 0 : 1)
+
 
             }
             Spacer()
 
         }
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-
         .onAppear{
 
             //if the saved data is not the same as the selected day, load new weather from the API
@@ -117,6 +116,46 @@ struct ForecastListView: View {
         }
     }
 
+    var imageURL:URL? {
+        //Note: URL from the API needs https added
+        guard let weather = forecastItem.weather, let condition = weather.condition(), let imageURL = URL(string: condition.icon.replacingOccurrences(of: "//", with: "https://")) else {
+            return nil
+        }
+        return imageURL
+    }
+
+    var conditionText:String {
+        guard let weather = forecastItem.weather, let condition = weather.condition() else {
+            return " "
+        }
+        return condition.text
+    }
+
+    var temperature:String {
+        guard let weather = forecastItem.weather, let temperature = weather.temperature() else {
+            return ""
+        }
+        return "\(Int(temperature))°"
+    }
+
+    var wind:String {
+        guard let weather = forecastItem.weather, let wind = weather.wind() else {
+            return ""
+        }
+        return "\(Int(wind)) mph"
+    }
+
+    var roof:String {
+        var roof = ""
+        if forecastItem.stadium.roof.uppercased() == "OPEN"{
+            roof = forecastItem.stadium.roof.uppercased()
+        } else {
+            roof = "\(forecastItem.stadium.roof.uppercased()) ROOF"
+        }
+        return roof
+    }
+
+
     //Using SwiftDate to compare dates. It's one of my favorite packages.
     func isSameDay(_ dateA: Date, _ dateB: Date) -> Bool {
         let result = dateA.compare(toDate: dateB, granularity: .day)
@@ -135,7 +174,7 @@ struct ForecastListView: View {
     var previewItem = ForecastItem(stadium: stadium)
 
     let condition:Condition = Condition(text: "Sunny", icon: "//cdn.weatherapi.com/weather/64x64/day/113.png", code: 1000)
-    let dayWeather:DayWeather = DayWeather(avgtempF: 57.8, maxwindMph: 7.8, condition: condition)
+    let dayWeather:DayWeather = DayWeather(avgtempF: 57.8, maxwindMph: 8.8, condition: condition)
     let forecastDay:ForecastDay = ForecastDay(day: dayWeather, dateEpoch: 1707004800)
     let forecast:Forecast = Forecast(forecastday: [forecastDay])
     let weather:Weather = Weather(forecast: forecast)
